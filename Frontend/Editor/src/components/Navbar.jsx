00488f63e93eb9675b4cb950582050f2d39b33c9
AmaySharma06 <amaysharma14102006@gmail.com>
2025-07-22 08:58:34 +0530

    Fixed navbar issue

---
+++ Frontend/Editor/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import '../styles/Navbar.css'
+
+function NavBar() {
+
+    return (
+        <nav>
+            <div className="nav-left">
+                <h2>Blog Publisher</h2>
+            </div>
+            <div className="nav-middle">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/new'>New Blog</NavLink>
+                <a href="https://github.com/AmaySharma06/blog-app" target="blank">GitHub</a>
+            </div>
+            <div className="nav-right">
+                <span>{localStorage.getItem("username")}</span>
+                <NavLink to='/login'><button>Log Out</button></NavLink>
+            </div>
+        </nav>
+
+    )
+}
+
+export default NavBar;
